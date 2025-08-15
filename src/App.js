3f81aa1fd8b3a2fc45e3eb721165b54cf652be51
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-08-15 05:16:44 -0700

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hops Mieng</h1>
+      <p>Ứng dụng quản lý và theo dõi quá trình thu hoạch và phân phối hạt cà phê.</p>
+    </div>
+  );
+}
+
+export default App;
