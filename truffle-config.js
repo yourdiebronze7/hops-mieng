d12630450db0854b13e8e2ef7b3e5ace68ecd3d3
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-08-15 05:16:52 -0700

    feat: initial commit for truffle-config.js

---
+++ truffle-config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0"
+    }
+  }
+};
