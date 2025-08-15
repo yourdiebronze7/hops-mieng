ae3d2979962da77d17ac648bd90eade60aa3cd7a
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-08-15 05:32:50 -0700

    Add Vietnamese comments to truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,21 @@
 const path = require('path');
 
 module.exports = {
+  // Đường dẫn nơi chứa các hợp đồng biên soạn
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
+      // Địa chỉ host cho mạng phát triển
       host: "127.0.0.1",
+      // Cổng cho mạng phát triển
       port: 7545,
+      // ID mạng, dùng để xác định mạng
       network_id: "*"
     }
   },
   compilers: {
     solc: {
+      // Phiên bản của trình biên dịch Solidity
       version: "0.8.0"
     }
   }
